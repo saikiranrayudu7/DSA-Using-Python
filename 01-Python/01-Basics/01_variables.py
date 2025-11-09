8df34f6ff95179214f89199bd0419b8ac9d51158
saikirun7 <saikirunrayudu@gmail.com>
2025-11-09 12:16:57 +0530

    # Nov 9th 2025 Variables

---
+++ 01-Python/01-Basics/01_variables.py
@@ -0,0 +1,34 @@
+# Nov 9th 2025 Variables, Data types, Operators
+
+#Variables
+x = 10
+y = "Sai"
+print(x)
+print(y)
+
+print(str(x) + y)
+print(str(x) + " " + y)
+
+#Variable will get override
+m = 10
+m = "Sai"
+print(m)
+
+#To check variable data type
+m = 10
+n = "Sai"
+print(type(m))
+print(type(n))
+
+#We can declare string variables either double quotes "" or single quotes ''
+m = "Sai"
+n = 'Sai'
+print(m)
+print(n)
+
+#Declare same variables with case-sensitive, it will take as different variable
+a = 10
+A = "Sai"
+print(a)
+print(A)
+
